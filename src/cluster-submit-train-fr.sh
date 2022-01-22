8338e7d65a2b1b6c8944bd82b88bb52ea465d6a2
Qihong Lu <7212521+qihongl@users.noreply.github.com>
2022-01-22 14:06:35 -0500

    fix default

--- src/cluster-submit-train-fr.sh
+++ src/cluster-submit-train-fr.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 exp_name=free-recall
+reward=1
 penalty=-.5
-n=6
-n_std=20
+penalize_repeat=1
+
 lr=1e-3
 n_epochs=200001
-reward=1
-penalize_repeat=1
+
+n_std=6
 
 for subj_id in {0..3}
 do
    for dim_hidden in 64 512 2048
    do
+     for n in 20
+     do
          sbatch cluster-train-fr.sh \
          ${exp_name} ${subj_id} ${n} ${n_std} ${dim_hidden} \
          ${lr} ${n_epochs} ${reward} ${penalty} ${penalize_repeat}
+       done
    done
 done
